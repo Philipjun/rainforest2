d199ffc532421a3bf4299a9be6830fabe0cb52e1
Philip Jun <inbox@philipjun.com>
2014-04-30 10:07:00 -0400

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Product.destroy_all
+
+User.create!({
+  email: "1@1.com",
+  name: "One",
+  password: "1",
+  password_confirmation: "1"
+  }
+  )
+
+100.times do
+  Product.create(
+    name: Faker::Company.catch_phrase,
+    description: "#{Faker::Company.bs} from #{Faker::Company.name} #{Faker::Company.suffix}",
+    price_in_cents: rand(100000)
+  )
+end
